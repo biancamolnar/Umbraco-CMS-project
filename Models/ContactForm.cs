c9b1597ab9efe4bce7cb2ee3494490fef49f600b
Bianca Molnar <biancascorreos@hotmail.de>
2023-10-08 18:47:56 +0200

    last fixes

--- Models/ContactForm.cs
+++ Models/ContactForm.cs
@@ -4,14 +4,14 @@ namespace Crito.Models;
 
 public class ContactForm
 {
-    [Required]
+    [Required(ErrorMessage = "Name is required.")]
     public string Name { get; set; } = null!;
 
-    [Required]
-    [EmailAddress]
+    [Required(ErrorMessage = "Email is required.")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid Email Address.")]
     public string Email { get; set; } = null!;
 
-    [Required]
+    [Required(ErrorMessage = "Message is required.")]
     public string Message { get; set; } = null!;
 
     public string? RedirectUrl { get; set; } = "/";
